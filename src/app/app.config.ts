18222dbdca992f5ca582d966c093d403eb06db72
David Cárdenas <cardenasp456@gmail.com>
2025-06-11 17:26:47 -0500

    Feat: responsive navigation, language switch, and animated logo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,17 @@ import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslation } from './translate.config';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideAnimationsAsync(),
-        providePrimeNG({
+      provideHttpClient(),
+      provideAnimationsAsync(),
+      provideTranslation(),
+      providePrimeNG({
             theme: {
                 preset: Aura,
-                options: {
-                    darkModeSelector: '.my-app-dark'
-                }
             }
         })
     ]
